c4b5f4dd3cf60bd4c58a48bdcbb8885532f5f898
chylli-binary <chylli@binary.com>
2022-10-01 03:07:59 +0000

    initial commit

---
+++ lib/Sample/Public/Module.pm
@@ -0,0 +1,36 @@
+package Sample::Public::Module;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Sample::Public::Module - Module abstract
+
+=head1 SYNOPSIS
+
+    use Sample::Public::Module;
+    my $instance = Sample::Public::Module->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
